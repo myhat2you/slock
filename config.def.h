d7e14f650d22fd58311c2a4056951e7cd7a8427f
Myhat2you <talbot@hackersec.xyz>
2024-11-19 01:48:26 +0000

    applied patches

--- config.def.h
+++ config.def.h
@@ -3,10 +3,21 @@ static const char *user  = "nobody";
 static const char *group = "nogroup";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "black",     /* after initialization */
+	[BG] =     "black",     /* background */
+	[INIT] =   "#4f525c",   /* after initialization */
 	[INPUT] =  "#005577",   /* during input */
 	[FAILED] = "#CC3333",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
+
+/*
+* Shapes:
+* 0: square
+* 1: circle
+*/
+static const int shape = 0;
+/* size of square in px */
+static const int shapesize = 50;
+static const int shapegap = 35;
